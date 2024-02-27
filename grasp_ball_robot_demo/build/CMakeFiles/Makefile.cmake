a7548100484f635292c5c747a494349d55359ad1
KimDoYoung1997 <ehdud4520@naver.com>
2024-02-27 14:52:16 +0900

    before ports adding

--- grasp_ball_robot_demo/build/CMakeFiles/Makefile.cmake
+++ grasp_ball_robot_demo/build/CMakeFiles/Makefile.cmake
@@ -48,6 +48,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/grasp_place_robot_demo_ports.dir/DependInfo.cmake"
   "CMakeFiles/docking.dir/DependInfo.cmake"
   "CMakeFiles/grasp_place_robot_demo.dir/DependInfo.cmake"
   )
---
+++ grasp_ball_robot_demo/build/CMakeFiles/grasp_place_robot_demo_ports.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grasp_place_robot_demo_ports.dir/bt_demo_ports.cpp.o"
+  "grasp_place_robot_demo_ports"
+  "grasp_place_robot_demo_ports.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grasp_place_robot_demo_ports.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
